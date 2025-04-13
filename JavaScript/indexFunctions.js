5b663b7a79c2372e74eda48a0217295cedad5af0
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-04-13 23:17:45 +0300

    Update indexFunctions.js
    Added The Discord Server's Link

--- JavaScript/indexFunctions.js
+++ JavaScript/indexFunctions.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("github")
-    .addEventListener("click", function () {
-      window.location.href = "https://github.com/NFSHubster/PS-Vita-Data-Files";
-    });
+  document
+    .getElementById("github")
+    .addEventListener("click", function () {
+      window.location.href = "https://github.com/NFSHubster/PS-Vita-Data-Files";
+    });
+
+  document
+    .getElementById("discord")
+    .addEventListener("click", function () {
+      window.location.href = "https://discord.gg/dvdkWYMTz2";
+    });
 });
