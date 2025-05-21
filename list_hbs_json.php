3991c0e2ec82baa0311c19f3adc82a835bc551e6
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-05-21 13:11:54 +0300

    Update list_hbs_json.php

--- list_hbs_json.php
+++ list_hbs_json.php
@@ -22,5 +22,41 @@
   "long_description": "",
   "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=725",
   "data": "https://ia600408.us.archive.org/19/items/psv-homebrew/Data%20Files/Final%20Fantasy%203%20%28ff3%29.zip"
-}
+},
+{
+  "name": "Final Fantasy 4",
+  "version": "v.1.3.1",
+  "author": "Rinnegatamante",
+  "description": "A wrapper/port of Final Fantasy 4 for PS Vita.",
+  "date": "2023-09-05",
+  "titleid": "FF4000000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=726",
+  "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Final%20Fantasy%204%20%28ff4%29.zip"
+},
+{
+  "name": "Final Fantasy 4 The After Years",
+  "version": "v.1.0.1",
+  "author": "Rinnegatamante",
+  "description": "A wrapper/port of Final Fantasy 4 The After Years for PS Vita.",
+  "date": "2023-09-05",
+  "titleid": "FF4AY0000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=989",
+  "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Final%20Fantasy%20After%20Years%28ff4a%29.zip"
+},
+{
+  "name": "Final Fantasy 5",
+  "version": "v.1.0.1",
+  "author": "frangarcj",
+  "description": "A wrapper/port of Final Fantasy 5 for PS Vita.",
+  "date": "2023-09-05",
+  "titleid": "FF5000000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=733",
+  "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Final%20Fantasy%205%20%28ff5%29.zip"
+},
 ]
