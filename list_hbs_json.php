ad6d855b132b4d699392129f4dae1291abae9f16
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-05-21 20:33:26 +0300

    Update list_hbs_json.php

--- list_hbs_json.php
+++ list_hbs_json.php
@@ -63,7 +63,7 @@
   "name": "Simpsons Hit & Run",
   "version": "v.1.0",
   "author": "ZenoArrows",
-  "description": "A wrapper/port of Mass Effect Infiltrator for the PS Vita.",
+  "description": "A port of Simpsons Hit & Run for the PS Vita.",
   "date": "2022-11-21",
   "titleid": "SIMPSONSH",
   "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
