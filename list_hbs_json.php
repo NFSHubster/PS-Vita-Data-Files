52aeab4354d2bd79d3b40e1312fa6bafb7980125
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-05-21 12:56:40 +0300

    Update list_hbs_json.php

--- list_hbs_json.php
+++ list_hbs_json.php
@@ -1,14 +1,26 @@
 [
 {
-  "name": "Final Fantasy",
+  "name": "Final Fantasy Dimensions",
   "version": "v.1.2",
   "author": "Rinnegatamante",
-  "description": "Explore the world of Final Fantasy portably on the PlayStation Vita!",
-  "date": "2025-05-16",
-  "titleid": "CCFC12345",
+  "description": "A wrapper/port of Final Fantasy Dimensions for PS Vita.",
+  "date": "2024-03-27",
+  "titleid": "FFDIMEN01",
   "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
   "long_description": "",
-  "url": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=1188",
   "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Fina%20Fantasy%20Dimensions.zip"
+}
+{
+  "name": "Final Fantasy 3",
+  "version": "v.1.1",
+  "author": "frangarcj",
+  "description": "A wrapper/port of Final Fantasy 3 for PS Vita.",
+  "date": "2021-09-01",
+  "titleid": "FF3000000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=725",
+  "data": "https://ia600408.us.archive.org/19/items/psv-homebrew/Data%20Files/Final%20Fantasy%203%20%28ff3%29.zip"
 }
 ]
