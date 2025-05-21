7e7e1bb1ad5f1116b9678564ad63490a5a25696d
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-05-21 18:54:46 +0300

    Update list_hbs_json.php

--- list_hbs_json.php
+++ list_hbs_json.php
@@ -261,6 +261,18 @@
   "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Final%20Fantasy%205%20%28ff5%29.zip"
 },
 {
+  "name": "Baba Is You",
+  "version": "v.1.4",
+  "author": "atamenenko",
+  "description": "A wrapper/port of Baba is You to the PS Vita.",
+  "date": "2025-01-22",
+  "titleid": "QUAK00001",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=828",
+  "data": "https://archive.org/download/psv-homebrew/Data%20Files/Baba%20Is%20You.zip"
+},
+{
   "name": "Quake I",
   "version": "v.4.1",
   "author": "Rinnegatamante",
@@ -271,5 +283,29 @@
   "long_description": "",
   "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=10",
   "data": "https://dn721608.ca.archive.org/0/items/psv-homebrew/Data%20Files/Quake.zip"
+},
+{
+  "name": "Quake II",
+  "version": "v.2.4",
+  "author": "Rinnegatamante",
+  "description": "A port of Quake II to the PS Vita.",
+  "date": "2020-11-29",
+  "titleid": "QUAK20000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=278",
+  "data": "https://archive.org/download/psv-homebrew/Data%20Files/Quake2.zip"
+},
+{
+  "name": "Quake III Arena",
+  "version": "v.1.3",
+  "author": "Rinnegatamante",
+  "description": "A port of Quake III Arena to the PS Vita.",
+  "date": "2020-11-29",
+  "titleid": "QUAK20000",
+  "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
+  "long_description": "",
+  "url": "https://www.rinnegatamante.eu/vitadb/get_hb_url.php?id=375",
+  "data": "https://archive.org/download/psv-homebrew/Data%20Files/Quake2.zip"
 }
 ]
