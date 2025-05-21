29d0142cf2bd20a88e6e1065d0c268887e39890e
NFSHubster <ahmadshawkijaber.09@gmail.com>
2025-05-21 19:07:44 +0300

    Update list_hbs_json.php

--- list_hbs_json.php
+++ list_hbs_json.php
@@ -8,7 +8,8 @@
   "titleid": "BALATROPS",
   "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
   "long_description": "",
-  "url": "https://archive.org/download/dfr_2009/Balatro%20V0.13.vpk"
+  "url": "https://archive.org/download/dfr_2009/Balatro%20V0.13.vpk",
+  "data": ""
 },
 {
   "name": "Hollow Knight",
@@ -31,7 +32,8 @@
   "titleid": "CPHD00001",
   "screenshots": "screenshots/64e51442e44094b74e771c794501b4f45ca990c7df1dfec687b36d1e2411f76f.png;screenshots/fb818a377526f055a811f8a9abab780fa2c74a323163616627c9216050a79945.png;screenshots/6a75d3201ee1a12dbfe2c21b24af0017e9dab6a107501156aa9d5f72ef4611f0.png;screenshots/528c931df6640b07bbaff6b6947dedbde94e71bd9bd4ded2a37c6f66a72690b2.png",
   "long_description": "",
-  "url": "https://archive.org/download/dfr_2009/Cuphead%20V0.82.vpk"
+  "url": "https://archive.org/download/dfr_2009/Cuphead%20V0.82.vpk",
+  "data": ""
 },
 {
   "name": "Mass Effect Infiltrator",
